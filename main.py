0c945af7cd8277f7e28f640e7921fe138091c0d3
Nilli <maeedeh.khoshgoo@Gmail.com>
2024-12-19 01:01:30 +0330

    Add sorting code for students

--- main.py
+++ main.py
@@ -1,13 +1,12 @@
+
 students = [
-{"Firstname": "Maede", "Lastname": "Khoshgoo", "SID": 1399012268050},
-{"Firstname": "Anahid", "Lastname": "Hakimi", "SID": 1401354589856},
-{"Firstname": "Sara", "Lastname": "Zarei", "SID": 1401679815464},
+    {"Firstname": "Maede", "Lastname": "Khoshgoo", "SID": 123},
+    {"Firstname": "Anahid", "Lastname": "Hakimi", "SID": 456},
+    {"Firstname": "Sara", "Lastname": "Zarei", "SID": 789},
 ]
 
 sorted_students = sorted(students, key=lambda x: (x['Firstname'], x['Lastname'], x['SID']))
 
-                                                  for student in sorted_students:
-                                                                   print(student)
+for student in sorted_students:
+    print(student)
 
-students.append({"Firstname":"Nilli","Lastname": "Rezaei","SID" :1398013389743}
-students.append({"Firstname":"Reza","Lastname": "Karimi","SID" :1400828486329}
